c78513e7edf25036466cabf70ee699227165f3a0
GREENFONTS <godwillonyewuchii@gmail.com>
2023-12-23 14:39:34 +0100

    chore: cors

--- index.js
+++ index.js
@@ -17,13 +17,10 @@ const run = async () => {
   const io = new Server(server, {
     cors: {
       origin: [
-        "http://localhost:3000",
-        "http://localhost:3001",
         "http://localhost:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5173",
         "https://giftscircle.netlify.app",
         "https://eventcircleadmin.netlify.app",
+        "https://eventcirco.com"
       ],
     },
   });
@@ -51,6 +48,7 @@ const run = async () => {
         "https://giftscircle.netlify.app",
         "http://localhost:5173",
         "https://eventcircleadmin.netlify.app",
+        "https://eventcirco.com",
       ],
     })
   );
